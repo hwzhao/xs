85993deb2117d775cdd8565fb215f2f429d10f89
gaojianye <gao369345486@gmail.com>
2017-04-16 01:58:20 +0800

    ['添加搜索历史，搜索流程']

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -6,15 +6,35 @@
         <div class="search-box">
           <i class="xs-icon xs-icon-search"></i>
           <input type="text" name="query" v-focus class="search-input" placeholder="搜索图书或作者" v-model="query">
-          <i class="xs-icon xs-icon-close" @click="query = ''" v-if="query!= ''"></i>
+          <i class="xs-icon xs-icon-close" @click="query = '';type = 0" v-if="query!= ''"></i>
         </div>
       </form>
       <a class="search-submit" :class="{'active':query.length}" name="button" @click="search">搜索</a>
     </header>
-    <ul class="auto after-boeder" v-show="keywords.length > 0">
+    <div class="history" v-show="type === 0">
+      <div class="header" v-if="searchList.length > 0">
+        搜索历史
+        <span @click="clear">清空</span>
+      </div>
+      <ul class="" v-if="searchList.length > 0">
+        <li v-for="(search, index) in searchList" :style="{'background-color': bColor[index]}" @click="autoClick(search)">
+          {{search}}
+          <i class="xs-icon xs-icon-close" @click.stop="del(index)"></i>
+        </li>
+      </ul>
+
+        <div class="" v-if="searchList.length == 0">
+          赶快搜索好书来看吧
+        </div>
+    </div>
+
+    <ul class="auto after-boeder" v-show="type === 1">
       <li v-for="keyword in keywords" class="after-boeder" @click="autoClick(keyword)">{{keyword}}</li>
     </ul>
-    <ul class="books after-boeder" v-show="type== 'search'">
+    <div class="" v-if="type === 2">
+      搜索中。。。。
+    </div>
+    <ul class="books after-boeder" v-show="type== 3">
       <li v-for="book in books" class="after-boeder" v-if="books.length > 0">
         <img :src="book.cover | cover" alt="">
         <div class="">
@@ -25,21 +45,31 @@
         </div>
       </li>
     </ul>
+
+    <div class="" v-if="type === 3 && books.length == 0">
+      我擦，找不到哎╮(╯▽╰)╭
+    </div>
   </div>
 </template>
 
 <script>
-import { apiAutocomplate, apiSearch } from '@/service/index.js'
+import { mapState } from 'vuex'
 export default {
   name: 'search',
   data () {
     return {
       query: '',
       books: [],
-      keywords: '',
+      keywords: [],
       autoQuert: '',
-      type: ''
+      type: 0,
+      bColor: ['#178df1', '#df1717', '#36b54e', '#febc2a', '#fd5d23', '#839eb1']
     }
+  },
+  computed: {
+    ...mapState({
+      'searchList': state => state.searchList
+    })
   },
   filters: {
     cover (value) {
@@ -47,10 +77,37 @@ export default {
     }
   },
   methods: {
+    del (index) {
+      let searchList = this.searchList
+      searchList.splice(index, 1)
+      this.$store.commit('setSearch', searchList)
+    },
+    clear () {
+      this.$store.commit('setSearch', [])
+    },
     search () {
+      this.query = this.query.replace(/(^\s*)|(\s*$)/g, '')
+      if (this.query === '') {
+        return ''
+      }
       const self = this
-      this.type = 'search'
-      apiSearch(self, this.query)
+      this.type = 2
+      this.$http.get(`/api/book/fuzzy-search`, {
+        params: {
+          'query': this.query,
+          'start': '0',
+          'limit': '4'
+        }
+      })
+      .then(function (response) {
+        self.books = response.data.books
+        if (self.searchList.indexOf(self.query) === -1) {
+          let searchList = self.searchList
+          searchList.push(self.query)
+          self.$store.commit('setSearch', searchList)
+        }
+        self.type = 3
+      })
       this.keywords = ''
     },
     autoClick (keyword) {
@@ -61,17 +118,24 @@ export default {
   },
   watch: {
     query (newvalue) {
+      newvalue = newvalue.replace(/(^\s*)|(\s*$)/g, '')
       this.books = ''
-      if (newvalue && this.autoQuert !== this.query) {
+      if (newvalue.length && this.autoQuert !== this.query) {
         const self = this
-        this.type = 'auto'
-        apiAutocomplate(self, this.query)
-      } else {
-        this.keywords = ''
+        this.type = 1
+        this.$http.get(`/api/book/auto-complete`, {
+          params: {
+            'query': this.query
+          }
+        })
+        .then(function (response) {
+          self.keywords = response.data.keywords
+        })
       }
     }
   },
   created () {
+    this.$store.commit('setSearch', JSON.parse(window.localStorage.getItem('searchList')))
   }
 }
 </script>
@@ -79,6 +143,33 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 @import "../styles/style.scss";
+  .history {
+    padding: 10px 20px;
+    .header {
+      text-align: left;
+      font-size: 12px;
+      span {
+        float: right;
+      }
+    }
+    ul {
+      padding: 10px 0;
+      margin-left: -10px;
+      li {
+        float: left;
+        padding: 5px 8px;
+        border-radius: 15px;
+        font-size: 14px;
+        color: #fff;
+        margin: 10px;
+        position: relative;
+
+        .xs-icon-close {
+          margin-left: 10px;
+        }
+      }
+    }
+  }
   .xs-search {
     display: flex;
     padding: 10px 0;
--- src/main.js
+++ src/main.js
@@ -4,6 +4,23 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store(
+  {
+    state: {
+      searchList: []
+    },
+    mutations: {
+      setSearch (state, search) {
+        state.searchList = search
+        window.localStorage.setItem('searchList', JSON.stringify(state.searchList))
+      }
+    }
+  }
+)
 
 Vue.directive('focus', {
   // 当绑定元素插入到 DOM 中。
@@ -15,12 +32,12 @@ Vue.directive('focus', {
 
 Vue.config.productionTip = true
 Vue.prototype.$http = axios
-console.log(axios)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
